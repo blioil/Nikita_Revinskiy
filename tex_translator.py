f434235a8b076e8b3d88c06a5603fdb56238e925
blioil <pasakoktys@gmail.com>
2026-07-22 18:52:25 +0300

    added tex_translator.py.py.py +

--- tex_translator.py
+++ tex_translator.py
@@ -1,20 +1,22 @@
 def number_to_english(n):
-    if n == 1:
-        return "one"
-    elif n == 2:
-        return "two"
-    elif n == 3:
-        return "three"
-    elif n == 4:
-        return "four"
-    elif n == 5:
-        return "five"
-    else:
-        return None
+    # Словарь способ сопоставления
+    number_dict = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five"
+    }
 
-n = int(input())
-word = number_to_english(n)
-if word is not None:
-    print(word)
+    return number_dict.get(n)
+
+try:
+    n = int(input("Введите число от 1 до 5: "))
+except ValueError:
+    print("Ошибка: нужно ввести целое число.")
 else:
-    print("Число должно содержать от 1 до 5")
+    word = number_to_english(n)
+    if word is not None:
+        print(word)
+    else:
+        print("Число должно содержать от 1 до 5")

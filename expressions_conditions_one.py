1435d24d6b3efbbb0e20cb9dbbc041cb6da6b2c1
blioil <pasakoktys@gmail.com>
2026-07-22 18:43:17 +0300

    added expressions_conditions_one.py.py +

--- expressions_conditions_one.py
+++ expressions_conditions_one.py
@@ -1,6 +1,8 @@
 def can_vote(user_age, user_citizen, user_disqualified):
+    if user_age < 0:
+        return False, "Возраст не может быть отрицательным."
     if user_age < 18:
-        return False, "Вам должно быть 18 лет или старше"
+        return False, "Вам должно быть 18 лет или старше."
     if not user_citizen:
         return False, "Вы должны быть гражданином страны"
     if user_disqualified:
@@ -8,9 +10,24 @@ def can_vote(user_age, user_citizen, user_disqualified):
     return True, "Вы можете голосовать!"
 
 # Ввод данных
-user_age = int(input("Введите ваш возраст: "))
-user_citizen = input("Вы гражданин? (да/нет): ").lower() == "да"
-user_disqualified = input("Вы дисквалифицированы? (да/нет): ").lower() == "да"
+while True:
+    user_int = (input("Введите ваш возраст: "))
+    try:
+        user_age = int(user_int)
+        if user_age < 0:
+            print("Возраст не может быть отрицательным.")
+            continue
+        break
+    except ValueError:
+        print("Ошибка:\"Введите целое данные\"")
 
-can_vote_result, message = can_vote(user_age, user_citizen, user_disqualified)
+# Ввод гражданство
+citizen_input = input("Вы гражданин? (да/нет): ").strip().lower()
+user_citizen = citizen_input in ("да", "yes", "no", "нет")
+
+# Ввод дисквалификации
+disqual_input = input("Вы дисквалифицированы? (да/нет): ").strip().lower()
+user_disqualified = disqual_input in ("да", "yes", "no", "нет")
+
+message = can_vote(user_age, user_citizen, user_disqualified)
 print(message)

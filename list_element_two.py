f70cb54f4b1a4f9be81e92ce7eea60d6d785608e
blioil <pasakoktys@gmail.com>
2026-07-29 11:43:55 +0300

    added list_element.py.py.py +

--- list_element_two.py
+++ list_element_two.py
@@ -7,16 +7,14 @@ def elem_list(list_one, list_two):
         longer_list = list_one
         short_list = list_two
 
-    sum_list = []
-    # Суммируем элементы, пока есть пары
-    for i in range(len(short_list)):
-        sum_list.append(short_list[i] + longer_list[i])
-    # Добавляем оставшиеся элементы из длинного списка
-    for i in range(len(short_list), len(longer_list)):
-        sum_list.append(longer_list[i])
+
+    # Суммируем попарно элементы короткого и длинного списков
+    sum_list = [short_list[i] + longer_list[i] for i in range(len(short_list))]
+    # Добавляем оставшиеся элементы из длинного списка (если есть)
+    sum_list.extend(longer_list[len(short_list):])
     return sum_list
 
-
+# Проверка
 list_one = [3, 5, 7, 9, 11, 13]
 list_two = [1, 4, 6, 8]
 result = elem_list(list_one, list_two)
